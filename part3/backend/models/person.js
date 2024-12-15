a811e16f3e49cd867f8a77df8bdedf89653a0065
“gene-themachine” <98907843+gene-themachine@users.noreply.github.com>
2024-12-15 16:19:43 +0900

    added number as a requirement again-debugged.

--- part3/backend/models/person.js
+++ part3/backend/models/person.js
@@ -4,33 +4,36 @@ const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 
 
-mongoose.connect(url)
-  .then(() => {
-    console.log('connected to MongoDB')
+mongoose.connect(url) 
+    .then(() => {
+        console.log("connected to MongoDB")
 
-  })
+    })
 
-  .catch(error => {
-    console.log('error connecting to MongoDB', error.message)
+    .catch(error => {
+        console.log('error connecting to MongoDB', error.message)
 
-  })
+    })
 
 const personSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    minLength: 3,
-    required: true
-  },
-
-
+    name: {
+        type: String, 
+        minLength: 3,
+        required: true
+    },
+    number: {
+        type: String,
+        required: true, // Number is required
+      },
+    
 })
 
 personSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
 })
 
-module.exports = mongoose.model('person', personSchema)
+module.exports = mongoose.model("person", personSchema)
